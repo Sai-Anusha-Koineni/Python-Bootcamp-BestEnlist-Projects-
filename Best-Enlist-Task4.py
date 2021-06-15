cdc8701a77c14a228a8506782a94fdd2e2d224eb
Sai Anusha Koineni <62833673+Sai-Anusha-Koineni@users.noreply.github.com>
2021-06-15 12:11:27 +0530

    Add files via upload

---
+++ Best-Enlist-Task4.py
@@ -0,0 +1,24 @@
+#first program
+a=int(input("Enter the 1st number:"))
+b=int(input("Enter the 2nd number:"))
+c=int(input("Enter the 3rd number:"))
+
+a=a%10
+b=b*50
+c=c+60
+print(a)
+print(b)
+print(c)
+
+
+#second program
+string="A n u s h "
+print(string.replace('s','G'))
+
+#Third program
+a=int(input("Enter the number:"))
+b=float(input("Enter the float value:"))
+a=float(a)
+b=int(b)
+print(a)
+print(b)
